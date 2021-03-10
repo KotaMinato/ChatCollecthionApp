62f81d7e83b38a6bbcef7e25916d4e39741ebf50
kota minato <minato.innetwork@gmail.com>
2021-03-10 18:01:02 +0900

    コレクションビューの表示

---
+++ ChatCollecthionApp/CustomCell.swift
@@ -0,0 +1,8 @@
+//
+//  CustomCell.swift
+//  ChatCollecthionApp
+//
+//  Created by minato on 2021/03/10.
+//
+
+import Foundation
--- ChatCollecthionApp/ViewController.swift
+++ ChatCollecthionApp/ViewController.swift
@@ -7,13 +7,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+   
+    let photos = ["ダウンロード","0913","09131","good-news","のん","りか","ぶた"]
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            return photos.count // 表示するセルの数
+          
+        }
+            
+            func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+                let testCell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) // 表示するセルを登録(先程命名した"Cell")
+                
+          
+                let imageView = testCell.contentView.viewWithTag(1) as! UIImageView
+                let cellImage = UIImage(named: photos[indexPath.row])
+                imageView.image = cellImage
+                
+                let label = testCell.contentView.viewWithTag(2) as! UILabel
+                label.text = photos[indexPath.row]
+                return testCell
+            }
+   
 
+       func numberOfSections(in collectionView: UICollectionView) -> Int {
+             return 1
+         
+    }
+           
 
 }
+
+
+
 
